0d8fe71161c2697404bdfa4a74d229033bca9972
Z3US-PROGRAMADOR <144644076+Z3US-PROGRAMADOR@users.noreply.github.com>
2026-06-06 15:50:21 -0500

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -2,11 +2,11 @@ const supabaseUrl = "https://emhojhrdwiitlqkdblxa.supabase.co";
 
 const supabaseKey = "sb_publishable_M4ooOjAUewjS8p3r7G-KwA_Ks9Gmpr2";
 
-const supabase = window.supabase.createClient(
-    supabaseUrl,
-    supabaseKey
+// Cambiamos el nombre de la variable
+const supabaseClient = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
 );
 
-console.log("Supabase conectado correctamente");
-
-console.log(supabase);
+console.log("Conectado");
+console.log(supabaseClient);
