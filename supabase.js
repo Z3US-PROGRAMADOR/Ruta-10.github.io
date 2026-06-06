bc8dd94fb9b823c5140e628f0e83f71386559e99
Z3US-PROGRAMADOR <144644076+Z3US-PROGRAMADOR@users.noreply.github.com>
2026-06-06 15:12:51 -0500

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,12 @@
+const supabaseUrl = "https://emhojhrdwiitlqkdblxa.supabase.co/rest/v1/";
+
+const supabaseKey = "sb_publishable_M4ooOjAUewjS8p3r7G-KwA_Ks9Gmpr2";
+
+const supabase = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
+
+console.log("Supabase conectado correctamente");
+
+console.log(supabase);
